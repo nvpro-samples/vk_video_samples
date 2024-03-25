598e418a18782a099286ba4992c6bfc7ed272ca1
Tymur Boiko <tboiko@nvidia.com>
2024-03-26 00:08:25 +0200

    Provide a simd-based version for the next_start_code function
    - Details:
    Added avx-512 implementation.

--- vk_video_decoder/libs/NvVideoParser/include/VulkanVideoDecoder.h
+++ vk_video_decoder/libs/NvVideoParser/include/VulkanVideoDecoder.h
@@ -151,6 +151,7 @@ protected:
 
 protected:
     // Byte stream parsing
+    size_t next_start_code_old(const uint8_t *pdatain, size_t datasize, bool& found_start_code);
     size_t next_start_code(const uint8_t *pdatain, size_t datasize, bool& found_start_code);
     void nal_unit();
     void init_dbits();
