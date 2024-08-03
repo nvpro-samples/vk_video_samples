a0d32fc2d173eb7a10a1a2d050466ae3285ab14c
Tymur Boiko <tboiko@nvidia.com>
2024-08-03 17:10:10 +0000

    Improvements of next_start_code
    Details:
    - Fixed bug in AVX2 code;

--- vk_video_decoder/libs/NvVideoParser/src/VulkanVideoDecoder.cpp
+++ vk_video_decoder/libs/NvVideoParser/src/VulkanVideoDecoder.cpp
@@ -360,7 +360,7 @@ size_t VulkanVideoDecoder::next_start_code<SIMD_ISA::AVX2>(const uint8_t *pdatai
         const __m256i v1 = _mm256_set1_epi8(1);
         __m256i vdata = _mm256_loadu_si256((__m256i*)pdatain);
         __m256i vBfr = _mm256_set1_epi16(((m_BitBfr << 8) & 0xFF00) | ((m_BitBfr >> 8) & 0xFF));
-        __m256i vdata_alignr16b_init = _mm256_permute2f128_si256(vdata, vBfr,  2);
+        __m256i vdata_alignr16b_init = _mm256_permute2f128_si256(vBfr, vdata, 1 | (2<<4));
         __m256i vdata_prev1 = _mm256_alignr_epi8(vdata, vdata_alignr16b_init, 15);
         __m256i vdata_prev2 = _mm256_alignr_epi8(vdata, vdata_alignr16b_init, 14);
         for ( ; i < datasize64 - 64; i += 64)
@@ -381,7 +381,7 @@ size_t VulkanVideoDecoder::next_start_code<SIMD_ISA::AVX2>(const uint8_t *pdatai
                 }
                 // hotspot begin
                 __m256i vdata_next = _mm256_loadu_si256((__m256i*)&pdatain[i + c + 32]); // 7-8 clocks
-                __m256i vdata_alignr16b_next = _mm256_permute2f128_si256(vdata_next, vdata, 1+(2<<4));
+                __m256i vdata_alignr16b_next = _mm256_permute2f128_si256(vdata, vdata_next, 1 | (2<<4));
                 vdata_prev1 = _mm256_alignr_epi8(vdata_next, vdata_alignr16b_next, 15);
                 vdata_prev2 = _mm256_alignr_epi8(vdata_next, vdata_alignr16b_next, 14);
                 vdata = vdata_next;
