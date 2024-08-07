839a80f0c83df6aa6d2ce85c361ea691cce6e5f5
Tymur Boiko <tboiko@nvidia.com>
2024-08-07 20:29:51 +0000

    Improvements of next_start_code
    Details:
    - The code was refactored;
    - Moved to the separate compilation of next_start_code function's specializations.

--- vk_video_decoder/libs/NvVideoParser/include/cpudetect.h
+++ vk_video_decoder/libs/NvVideoParser/include/cpudetect.h
@@ -11,6 +11,17 @@ enum SIMD_ISA
     SVE
 };
 
+static int inline count_trailing_zeros(unsigned long long resmask)
+{
+#ifndef _WIN32
+    int offset = __builtin_ctzll(resmask);
+#else
+    unsigned long offset = 0;
+    const unsigned char dummyIsNonZero =_BitScanForward64(&offset, resmask); // resmask can't be 0 in this if
+#endif
+    return offset;
+}
+
 SIMD_ISA check_simd_support();
 
 #endif
