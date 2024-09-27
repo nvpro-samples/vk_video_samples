nvpro-samples/vk_video_samples

commit 87aa1d7794a0eead879e512ad58bea3b94c78fc4

decode: Add AV1 decode support
Signed-off-by: Vassili Nikolaev (NVIDIA) <vnikolaev@nvidia.com>

vk_video_decoder/libs/NvVideoParser/src/VulkanH265Parser.cpp
@@ -147,7 +147,7 @@ bool VulkanH265Decoder::BeginPicture(VkParserPictureData *pnvpd)
     pnvpd->progressive_frame = 1;
     pnvpd->top_field_first = 0;
     pnvpd->repeat_first_field = 0;
-    pnvpd->ref_pic_flag = 1;    // TBD
+    pnvpd->ref_pic_flag = m_slh.nal_unit_type < 16 ? m_slh.nal_unit_type & 1 : 1;
     pnvpd->intra_pic_flag = m_intra_pic_flag;
     pnvpd->chroma_format = sps->chroma_format_idc;
     pnvpd->picture_order_count = cur->PicOrderCntVal << 1;
@@ -176,6 +176,7 @@ bool VulkanH265Decoder::BeginPicture(VkParserPictureData *pnvpd)
 
     hevc->IrapPicFlag = m_slh.nal_unit_type >= NUT_BLA_W_LP && m_slh.nal_unit_type <= NUT_CRA_NUT;
     hevc->IdrPicFlag = m_slh.nal_unit_type == NUT_IDR_W_RADL || m_slh.nal_unit_type == NUT_IDR_N_LP;
+    hevc->short_term_ref_pic_set_sps_flag = m_slh.short_term_ref_pic_set_sps_flag;
 
     // ref pic sets
     hevc->CurrPicOrderCntVal = cur->PicOrderCntVal;

commit 9f3e6272b3e610cce166e93ce9ec6ae7c9e1b70c

decoder: revert fix HEVC parser refpic flag
Fix h.265 regression introduced from 87aa1d7794a0eead879e512ad58bea3b94c78fc4
refpic flag in HEVC must always be set.

vk_video_decoder/libs/NvVideoParser/src/VulkanH265Parser.cpp
@@ -147,7 +147,7 @@ bool VulkanH265Decoder::BeginPicture(VkParserPictureData *pnvpd)
     pnvpd->progressive_frame = 1;
     pnvpd->top_field_first = 0;
     pnvpd->repeat_first_field = 0;
-    pnvpd->ref_pic_flag = m_slh.nal_unit_type < 16 ? (m_slh.nal_unit_type & 1) : 1;
+    pnvpd->ref_pic_flag = 1; // We enforce the ref_pic_flag setting in Vulkan Video..
     pnvpd->intra_pic_flag = m_intra_pic_flag;
     pnvpd->chroma_format = sps->chroma_format_idc;
     pnvpd->picture_order_count = cur->PicOrderCntVal << 1;