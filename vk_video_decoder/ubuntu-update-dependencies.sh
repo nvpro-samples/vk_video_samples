68c52a4953e753ffab9c26185b2dbce7490b72a9
Tony Zlatinski <tzlatinski@nvidia.com>
2025-01-15 15:38:27 -0600

    Remove explicit shaderc and tools

--- vk_video_decoder/ubuntu-update-dependencies.sh
+++ vk_video_decoder/ubuntu-update-dependencies.sh
@@ -1,1 +1,1 @@
-sudo apt-get install libavcodec-dev libavformat-dev libavutil-dev libwayland-dev
+sudo apt-get install libavcodec-dev libavformat-dev libavutil-dev libwayland-dev shaderc spirv-tools vulkan-headers
