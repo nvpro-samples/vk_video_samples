nvpro-samples/vk_video_samples

commit 16607feb9b86984b53e1747332c1ca386d7ecd2d

encoder: Deal with the Vulkan chained stuctures

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoder.h
@@ -58,7 +58,7 @@ public:
     {
         VkStructureType GetType() {
             return (encodeInfo.pNext == nullptr) ?
-                    VK_STRUCTURE_TYPE_VIDEO_ENCODE_INFO_KHR : ((VkBaseInStructure*)encodeInfo.pNext)->sType;
+                    VK_STRUCTURE_TYPE_VIDEO_ENCODE_INFO_KHR : reinterpret_cast<const VkBaseInStructure*>(encodeInfo.pNext)->sType;
         }
 
         VkVideoEncodeFrameInfo(const void* pNext = nullptr)

common/libs/VkCodecUtils/Helpers.h
@@ -319,7 +319,7 @@ inline VkResult WaitAndGetStatus(const VkInterfaceFunctions* vkIf, VkDevice devi
  }
 
 template<typename NodeType, typename ChainedNodeType>
-inline VkBaseInStructure* ChainNextVkStruct(NodeType& node, ChainedNodeType& nextChainedNode) {
+inline void ChainNextVkStruct(NodeType& node, ChainedNodeType& nextChainedNode) {
     // make sure the node is of type VkBaseInStructure
     static_assert(offsetof(NodeType, sType) == offsetof(VkBaseInStructure, sType),
                   "NodeType does not have sType at the same offset as VkBaseInStructure");
@@ -340,16 +340,16 @@ inline VkBaseInStructure* ChainNextVkStruct(NodeType& node, ChainedNodeType& nex
                   "ChainedNodeType must be a standard-layout type");
 
     assert(node.sType > 0);
-    VkBaseInStructure* pNode = (VkBaseInStructure*)&node;
-    while (pNode->pNext != nullptr) {
-         pNode = (VkBaseInStructure*)pNode->pNext;
-     }
-     pNode->pNext = (VkBaseInStructure*)&nextChainedNode;
-     // make sure the nextChainedNode is of type VkBaseInStructure
-     assert(nextChainedNode.sType > 0);
-     assert(nextChainedNode.pNext == nullptr);
-     return (VkBaseInStructure*)nextChainedNode.pNext;
- }
+    VkBaseInStructure* pNode = (VkBaseInStructure*)(&node);
+    VkBaseInStructure* pNextNode = (VkBaseInStructure*)(&nextChainedNode);
+
+    // The incoming object may not have anything chained.
+    assert(pNextNode->pNext == nullptr);
+
+    // Inserts the incoming object at the beginning of the list.
+    pNextNode->pNext = pNode->pNext;
+    pNode->pNext = pNextNode;
+}
 
 class DeviceUuidUtils
 {

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoder.cpp
@@ -1554,12 +1554,9 @@ VkResult VkVideoEncoder::HandleCtrlCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& 
         encodeFrameInfo->qualityLevelInfo.sType  = VK_STRUCTURE_TYPE_VIDEO_ENCODE_QUALITY_LEVEL_INFO_KHR;
         encodeFrameInfo->qualityLevelInfo.qualityLevel = encodeFrameInfo->qualityLevel;
         if (pNext != nullptr) {
-            if (encodeFrameInfo->rateControlInfo.pNext == nullptr) {
-                encodeFrameInfo->rateControlInfo.pNext = pNext;
-            } else {
-                ((VkBaseInStructure*)(encodeFrameInfo->rateControlInfo.pNext))->pNext = pNext;
-            }
+            vk::ChainNextVkStruct(encodeFrameInfo->rateControlInfo, *pNext);
         }
+
         pNext = (VkBaseInStructure*)&encodeFrameInfo->qualityLevelInfo;
     }
 
@@ -1582,12 +1579,9 @@ VkResult VkVideoEncoder::HandleCtrlCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& 
         m_beginRateControlInfo = encodeFrameInfo->rateControlInfo;
 
         if (pNext != nullptr) {
-            if (encodeFrameInfo->rateControlInfo.pNext == nullptr) {
-                encodeFrameInfo->rateControlInfo.pNext = pNext;
-            } else {
-                ((VkBaseInStructure*)(encodeFrameInfo->rateControlInfo.pNext))->pNext = pNext;
-            }
+            vk::ChainNextVkStruct(encodeFrameInfo->rateControlInfo, *pNext);
         }
+
         pNext = (VkBaseInStructure*)&encodeFrameInfo->rateControlInfo;
     }
 
@@ -1667,7 +1661,8 @@ VkResult VkVideoEncoder::RecordVideoCodingCmd(VkSharedBaseObj<VkVideoEncodeFrame
         vkDevCtx->CmdControlVideoCodingKHR(cmdBuf, &renderControlInfo);
 
         m_beginRateControlInfo = *(VkVideoEncodeRateControlInfoKHR*)encodeFrameInfo->pControlCmdChain;
-        ((VkBaseInStructure*)(m_beginRateControlInfo.pNext))->pNext = NULL;
+        // Do not walk the chain, otherwise we end up creating a loop here.
+        m_beginRateControlInfo.pNext = (VkBaseInStructure*)(&encodeFrameInfo->pControlCmdChain);
     }
 
     if (m_videoMaintenance1FeaturesSupported)
@@ -1679,10 +1674,12 @@ VkResult VkVideoEncoder::RecordVideoCodingCmd(VkSharedBaseObj<VkVideoEncodeFrame
         videoInlineQueryInfoKHR.firstQuery = querySlotId;
         videoInlineQueryInfoKHR.queryCount = numQuerySamples;
         VkBaseInStructure* pStruct = (VkBaseInStructure*)&encodeFrameInfo->encodeInfo;
-        while (pStruct->pNext) pStruct = (VkBaseInStructure*)pStruct->pNext;
-        pStruct->pNext = (VkBaseInStructure*)&videoInlineQueryInfoKHR;
+        vk::ChainNextVkStruct(*pStruct, videoInlineQueryInfoKHR);
 
         vkDevCtx->CmdEncodeVideoKHR(cmdBuf, &encodeFrameInfo->encodeInfo);
+
+        // Remove the stack pointer from the chain, causes a use after free otherwise in GetEncodeFrameInfoH264
+        encodeFrameInfo->encodeInfo.pNext = videoInlineQueryInfoKHR.pNext;
     }
     else
     {

commit 10606d84ea7d6e13c1bca4273cd703f4ccbe64a1

encode: improve reset of VkVideoEncodeInfoKHR.pNext chain
Prior to this commit, the code in VkVideoEncodeFrameInfo::Reset() was
written with the assumption that the first element in the pNext chain of
VkVideoEncodeInfoKHR would be a codec-specific structure describing the
picture info (e.g. VkVideoEncodeH264PictureInfoKHR). Correspondingly,
the overridden versions of ::Reset() in the derived classes would simply
do:

    pictureInfo.pNext = nullptr;

to clear the pNext chain.

Commit 16607feb9b86984b53e1747332c1ca386d7ecd2d regressed this by
changing the pNext chaining behaviour in the ChainNextVkStruct() helper
to add structures at the head of the pNext chain rather than at the
tail. This meant that the codec-specific picture info structure was not
guaranteed to be the first structure in the pNext chain of
VkVideoEncodeInfoKHR, so the above assignment was not sufficient to
clear the pNext chain.

This change updates VkVideoEncodeFrameInfo::Reset() to clear the pNext
of encodeInfo (i.e. VkVideoEncodeInfoKHR) and then have it point to
codec-specific picture info structure in the overridden ::Reset()
provided by the derived class. This change also restores the
functionality of quantization maps which was broken by commit
16607feb9b86984b53e1747332c1ca386d7ecd2d.

Signed-off-by: Srinath Kumarapuram <skr@nvidia.com>

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoder.h
@@ -246,7 +246,8 @@ public:
         virtual void Reset(bool releaseResources = true) {
             // Clear and check state
             assert(encodeInfo.sType == VK_STRUCTURE_TYPE_VIDEO_ENCODE_INFO_KHR);
-            assert(encodeInfo.pNext != nullptr);
+
+            encodeInfo.pNext = nullptr;
 
             if ((frameInputOrderNum == (uint64_t)-1) &&
                 (frameEncodeInputOrderNum == (uint64_t)-1) &&

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoderAV1.h
@@ -75,6 +75,9 @@ public:
 
             // Reset the base first
             VkVideoEncodeFrameInfo::Reset(releaseResources);
+
+            // After resetting the base structure parameters, start building the pNext chain again
+            encodeInfo.pNext = &pictureInfo;
         }
 
         virtual ~VkVideoEncodeFrameInfoAV1() {

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoderH264.h
@@ -78,6 +78,9 @@ public:
             // Reset the base first
             VkVideoEncodeFrameInfo::Reset(releaseResources);
 
+            // After resetting the base structure parameters, start building the pNext chain again
+            encodeInfo.pNext = &pictureInfo;
+
             // Clear and check state
             assert(pictureInfo.sType == VK_STRUCTURE_TYPE_VIDEO_ENCODE_H264_PICTURE_INFO_KHR);
             assert(naluSliceInfo.sType == VK_STRUCTURE_TYPE_VIDEO_ENCODE_H264_NALU_SLICE_INFO_KHR);

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoderH265.h
@@ -75,6 +75,9 @@ class VkVideoEncoderH265 : public VkVideoEncoder {
             // Reset the base first
             VkVideoEncodeFrameInfo::Reset(releaseResources);
 
+            // After resetting the base structure parameters, start building the pNext chain again
+            encodeInfo.pNext = &pictureInfo;
+
             // Clear and check state
             assert(pictureInfo.sType == VK_STRUCTURE_TYPE_VIDEO_ENCODE_H265_PICTURE_INFO_KHR);
             assert(naluSliceSegmentInfo.sType == VK_STRUCTURE_TYPE_VIDEO_ENCODE_H265_NALU_SLICE_SEGMENT_INFO_KHR);