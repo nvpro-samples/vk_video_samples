nvpro-samples/vk_video_samples

commit a97cf103a77b6784ac42fe08c043ef8d265964fc

decode: h.264 ensure PROGRESSIVE is set for progressive-only content
Signed-off-by: Tony Zlatinski <tzlatinski@nvidia.com>

vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfig.cpp
@@ -860,6 +860,7 @@ void EncoderConfig::InitVideoProfile()
                                           GetComponentBitDepthFlagBits(encodeBitDepthChroma),
                                           (videoProfileIdc != (uint32_t)-1) ? videoProfileIdc :
                                                   GetDefaultVideoProfileIdc(),
+                                                  VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_PROGRESSIVE_KHR, // interlaced video is supported with encode
                                           encodeUsageInfo);
 }
 

common/include/VkVideoCore/VkVideoCoreProfile.h
@@ -208,6 +208,8 @@ public:
                           VkVideoComponentBitDepthFlagsKHR lumaBitDepth = VK_VIDEO_COMPONENT_BIT_DEPTH_INVALID_KHR,
                           VkVideoComponentBitDepthFlagsKHR chromaBitDepth = VK_VIDEO_COMPONENT_BIT_DEPTH_INVALID_KHR,
                           uint32_t videoH26xProfileIdc = 0,
+                          VkVideoDecodeH264PictureLayoutFlagBitsKHR h264PictureLayout =
+                                  VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_PROGRESSIVE_KHR,
                           VkVideoEncodeUsageInfoKHR encodeUsageInfo =
                           { VK_STRUCTURE_TYPE_VIDEO_ENCODE_USAGE_INFO_KHR, NULL,
                           VK_VIDEO_ENCODE_USAGE_DEFAULT_KHR,
@@ -237,7 +239,7 @@ public:
             decodeH264ProfilesRequest.stdProfileIdc = (videoH26xProfileIdc == 0) ?
                                                        STD_VIDEO_H264_PROFILE_IDC_INVALID :
                                                        (StdVideoH264ProfileIdc)videoH26xProfileIdc;
-            decodeH264ProfilesRequest.pictureLayout = VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_INTERLACED_INTERLEAVED_LINES_BIT_KHR;
+            decodeH264ProfilesRequest.pictureLayout = h264PictureLayout;
             pVideoProfileExt = (VkBaseInStructure*)&decodeH264ProfilesRequest;
         } else if (videoCodecOperation == VK_VIDEO_CODEC_OPERATION_DECODE_AV1_BIT_KHR) {
             decodeAV1ProfilesRequest.sType = VK_STRUCTURE_TYPE_VIDEO_DECODE_AV1_PROFILE_INFO_KHR;

vk_video_decoder/include/vkvideo_parser/VulkanVideoParserIf.h
@@ -445,7 +445,8 @@ typedef struct VkParserSequenceInfo {
     uint8_t SequenceHeaderData[VK_MAX_SEQ_HDR_LEN];  // Raw sequence header data
                                                      // (codec-specific)
     uint32_t codecProfile;
-    bool hasFilmGrain;  // AV1 specific filmgrain.
+    uint32_t hasFilmGrain : 1;  // AV1 specific filmgrain.
+    uint32_t canUseFields : 1;  // H.264 only: can have field pictures. Otherwise the content has frames only to decode.
 } VkParserSequenceInfo;
 
 enum {

vk_video_decoder/include/vkvideo_parser/VulkanVideoParserParams.h
@@ -160,7 +160,8 @@ struct VkParserDetectedVideoFormat {
     } video_signal_description;
     uint32_t seqhdr_data_length; /** Additional bytes following (NVVIDEOFORMATEX)                  */
     uint32_t codecProfile;
-    bool filmGrainUsed;
+    uint32_t  filmGrainUsed : 1; // AV1 only, set when filmGrain is used with the content
+    uint32_t  canUseFields  : 1; // H.264 only: can have field pictures. Otherwise the content has frames only to decode.
 };
 
 typedef enum {

vk_video_decoder/libs/NvVideoParser/src/VulkanH264Parser.cpp
@@ -2714,6 +2714,7 @@ bool VulkanH264Decoder::dpb_sequence_start(slice_header_s *slh)
     nvsi.nCodedHeight = FrameHeightInMbs * 16;
     nvsi.nDisplayWidth = nvsi.nCodedWidth;
     nvsi.nDisplayHeight = nvsi.nCodedHeight;
+    nvsi.canUseFields  = !sps->flags.frame_mbs_only_flag;
     if (sps->flags.frame_cropping_flag)
     {
         int crop_right = sps->frame_crop_right_offset * 2;

vk_video_decoder/libs/VkVideoDecoder/VkVideoDecoder.cpp
@@ -139,7 +139,9 @@ int32_t VkVideoDecoder::StartVideoSequence(VkParserDetectedVideoFormat* pVideoFo
     }
 
     VkVideoCoreProfile videoProfile(videoCodec, pVideoFormat->chromaSubsampling, pVideoFormat->lumaBitDepth, pVideoFormat->chromaBitDepth,
-                                    pVideoFormat->codecProfile);
+                                    pVideoFormat->codecProfile, pVideoFormat->canUseFields ?
+                                        VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_INTERLACED_INTERLEAVED_LINES_BIT_KHR :
+                                        VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_PROGRESSIVE_KHR);
     if (!VulkanVideoCapabilities::IsCodecTypeSupported(m_vkDevCtx,
                                                        m_vkDevCtx->GetVideoDecodeQueueFamilyIdx(),
                                                        videoCodec)) {

vk_video_decoder/libs/VkVideoParser/VulkanVideoParser.cpp
@@ -1196,6 +1196,7 @@ int32_t VulkanVideoParser::BeginSequence(const VkParserSequenceInfo* pnvsi)
         detectedFormat.display_area.right = pnvsi->nDisplayWidth;
         detectedFormat.display_area.bottom = pnvsi->nDisplayHeight;
         detectedFormat.filmGrainUsed = pnvsi->hasFilmGrain;
+        detectedFormat.canUseFields  = pnvsi->canUseFields;
 
         if ((StdChromaFormatIdc)pnvsi->nChromaFormat == chroma_format_idc_420) {
             detectedFormat.chromaSubsampling = VK_VIDEO_CHROMA_SUBSAMPLING_420_BIT_KHR;

commit 564ca7f979ac3990879693d101b9fb1f63d542c2

encode: fix typo about interlaced encoding being supported
Encoding of interlaced content is not supported for any codec. The typo
in the comment was introduced by commit
a97cf103a77b6784ac42fe08c043ef8d265964fc.

This change also corrects the indentation for the affected line in the
code.

Signed-off-by: Srinath Kumarapuram <skr@nvidia.com>

vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfig.cpp
@@ -860,7 +860,7 @@ void EncoderConfig::InitVideoProfile()
                                           GetComponentBitDepthFlagBits(encodeBitDepthChroma),
                                           (videoProfileIdc != (uint32_t)-1) ? videoProfileIdc :
                                                   GetDefaultVideoProfileIdc(),
-                                                  VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_PROGRESSIVE_KHR, // interlaced video is supported with encode
+                                          VK_VIDEO_DECODE_H264_PICTURE_LAYOUT_PROGRESSIVE_KHR, // interlaced video is not supported with encode
                                           encodeUsageInfo);
 }
 