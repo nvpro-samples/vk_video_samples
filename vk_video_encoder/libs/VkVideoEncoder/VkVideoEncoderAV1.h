7a6ae69dadb173a95e60f20399eba8a037c5214b
Tony Zlatinski <tzlatinski@nvidia.com>
2025-11-27 15:20:09 -0600

    encoder: move common encoder code to EncodeFrameCommon
    Move common code from the codec-specific EncodeFrame to EncodeFrameCommon:
    - Assertions and basic encodeFrameInfo initialization
    - videoSession, videoSessionParameters, qualityLevel assignments
    - frameEncodeInputOrderNum increment and GOP position calculation
    - Control command handling (HandleCtrlCmd)
    - QP map processing (ProcessQpMap)
    - Intra refresh handling (FillIntraRefreshInfo)
    - Bitstream buffer acquisition (GetBitstreamBuffer)
    - Frame enqueuing (EnqueueFrame)
    
    Refactor rate control handling:
    - HandleCtrlCmd now returns bool indicating if rate control was sent
    - Add CodecHandleRateControlCmd pure virtual for codec-specific rate
      control chain setup (H264, H265, AV1 implement this)
    
    Remove bIsReference from AV1 codec-specific frame structure
    
    Additional changes:
    - Remove redundant bIsKeyFrame field from VkVideoEncodeFrameInfoAV1
      (replaced by direct gopPosition.pictureType checks)
    - Extract maxEncodeQueueDepth calculation into named const variable
    
    Signed-off-by: Tony Zlatinski <tzlatinski@nvidia.com>

--- vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoderAV1.h
+++ vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoderAV1.h
@@ -39,10 +39,8 @@ public:
         StdVideoAV1LoopRestoration stdLrInfo;
         bool bShowExistingFrame;
         int32_t frameToShowBufId;
-        bool bIsKeyFrame;
         bool bShownKeyFrameOrSwitch;
         bool bOverlayFrame;
-        bool bIsReference;
         StdVideoEncodeAV1ReferenceInfo stdReferenceInfo[STD_VIDEO_AV1_REFS_PER_FRAME];
         VkVideoEncodeAV1DpbSlotInfoKHR dpbSlotInfo[STD_VIDEO_AV1_REFS_PER_FRAME];
         VkVideoEncodeAV1RateControlInfoKHR rateControlInfoAV1;
@@ -59,10 +57,8 @@ public:
             , stdLrInfo{}
             , bShowExistingFrame{}
             , frameToShowBufId{-1}
-            , bIsKeyFrame{}
             , bShownKeyFrameOrSwitch{}
             , bOverlayFrame{}
-            , bIsReference{}
             , rateControlInfoAV1{ VK_STRUCTURE_TYPE_VIDEO_ENCODE_AV1_RATE_CONTROL_INFO_KHR }
             , rateControlLayersInfoAV1{{ VK_STRUCTURE_TYPE_VIDEO_ENCODE_AV1_RATE_CONTROL_LAYER_INFO_KHR }}
         {
@@ -155,8 +151,6 @@ public:
         return success;
     }
 
-    virtual VkResult EncodeFrame(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo);
-    virtual VkResult HandleCtrlCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo);
     virtual VkResult StartOfVideoCodingEncodeOrder(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo, uint32_t frameIdx, uint32_t ofTotalFrames);
     virtual VkResult RecordVideoCodingCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo,
                                          uint32_t frameIdx, uint32_t ofTotalFrames);
@@ -185,6 +179,9 @@ protected:
         }
     }
 
+    // Must be called from VkVideoEncoder::EncodeFrameCommon only
+    virtual VkResult EncodeFrame(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo);
+    virtual VkResult CodecHandleRateControlCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo);
 private:
     VkVideoEncodeFrameInfoAV1* GetEncodeFrameInfoAV1(VkSharedBaseObj<VkVideoEncodeFrameInfo>& encodeFrameInfo) {
         assert(VK_VIDEO_CODEC_OPERATION_ENCODE_AV1_BIT_KHR == encodeFrameInfo->GetType());
