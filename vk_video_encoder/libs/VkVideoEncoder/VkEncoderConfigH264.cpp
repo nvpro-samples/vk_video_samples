nvpro-samples/vk_video_samples

commit 7734964d130c3ec03a6b1daa71f7a7f0e729922b

encode: add EncoderConfigH264 method for profile and level determination
The codec profile and level are currently set in the following places:
- level in EncoderConfigH264::InitDpbCount()
- profile in EncoderConfigH264::InitSpsPpsParameters()

The profile and level are fairly basic encoding parameters and it is
desirable to set these as early as possible. This commit adds a method
to the EncoderConfigH264 class which sets the profile and level at an
early stage in the encoder configuration. This also provides the
opportunity to use the codec profile in the construction of the video
profile which will be used in physical device queries (currently, this
video profile uses a default codec profile).

The profile is more important than the level, so it is essential to set
these values in this order, as done within this change. Also, certain
level limits are profile-specific, and this ordering makes it possible
for a subsequent change to use the profile in the level determination
code.

Signed-off-by: Srinath Kumarapuram <skr@nvidia.com>

vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfigH264.cpp
@@ -287,23 +287,15 @@ bool EncoderConfigH264::InitSpsPpsParameters(StdVideoH264SequenceParameterSet *s
         sps->pic_order_cnt_type = STD_VIDEO_H264_POC_TYPE_2;
     }
 
-    // FIXME: Check if the HW supports transform_8x8_mode_is_supported
-    // based on capabilities or profiles supported
-    const bool transform_8x8_mode_is_supported = true;
-    const bool bIsFastestPreset = false;
-
     if (adaptiveTransformMode == ADAPTIVE_TRANSFORM_ENABLE) {
         pps->flags.transform_8x8_mode_flag = true;
     } else if (adaptiveTransformMode == ADAPTIVE_TRANSFORM_DISABLE) {
         pps->flags.transform_8x8_mode_flag = false;
     } else {
         // Autoselect
-        if (!bIsFastestPreset || transform_8x8_mode_is_supported) {
-            if ((profileIdc == STD_VIDEO_H264_PROFILE_IDC_INVALID) ||
-                (profileIdc >= STD_VIDEO_H264_PROFILE_IDC_HIGH)) {
-                // Unconditionally enable 8x8 transform
-                pps->flags.transform_8x8_mode_flag = true;
-            }
+        if (profileIdc >= STD_VIDEO_H264_PROFILE_IDC_HIGH) {
+            // Unconditionally enable 8x8 transform
+            pps->flags.transform_8x8_mode_flag = true;
         }
     }
 
@@ -318,28 +310,6 @@ bool EncoderConfigH264::InitSpsPpsParameters(StdVideoH264SequenceParameterSet *s
 
     if (constrained_intra_pred_flag) {
         pps->flags.constrained_intra_pred_flag = true;
-    }
-
-    // If the profileIdc hasn't been specified, force set it now.
-    if (profileIdc == STD_VIDEO_H264_PROFILE_IDC_INVALID) {
-        profileIdc = STD_VIDEO_H264_PROFILE_IDC_BASELINE;
-
-        if (entropyCodingMode == ENTROPY_CODING_MODE_CABAC) {
-            profileIdc = STD_VIDEO_H264_PROFILE_IDC_MAIN;
-        }
-
-        if ((gopStructure.GetConsecutiveBFrameCount() > 0) || pps->flags.entropy_coding_mode_flag || !sps->flags.frame_mbs_only_flag)
-            profileIdc = STD_VIDEO_H264_PROFILE_IDC_MAIN;
-
-        if (pps->flags.transform_8x8_mode_flag) {
-            profileIdc = STD_VIDEO_H264_PROFILE_IDC_HIGH;
-        }
-
-        if ((sps->flags.qpprime_y_zero_transform_bypass_flag &&
-             (rateControlMode == VK_VIDEO_ENCODE_RATE_CONTROL_MODE_DISABLED_BIT_KHR)) ||
-                (sps->chroma_format_idc == STD_VIDEO_H264_CHROMA_FORMAT_IDC_444)) {
-            profileIdc = STD_VIDEO_H264_PROFILE_IDC_HIGH_444_PREDICTIVE;
-        }
     }
 
     sps->profile_idc = profileIdc;
@@ -451,14 +421,43 @@ VkResult EncoderConfigH264::InitDeviceCapabilities(const VulkanDeviceContext* vk
     return VK_SUCCESS;
 }
 
-int8_t EncoderConfigH264::InitDpbCount()
+void EncoderConfigH264::InitProfileLevel()
 {
-    dpbCount = 0; // TODO: What is the need for this?
+    // FIXME: Check if the HW supports transform_8x8_mode_is_supported
+    // based on capabilities or profiles supported
+    bool use8x8Transform = true;
 
-    // spsInfo->level represents the smallest level that we require for the
-    // given stream. This level constrains the maximum size (in terms of
-    // number of frames) that the DPB can have. levelDpbSize is this maximum
-    // value.
+    if (adaptiveTransformMode == ADAPTIVE_TRANSFORM_ENABLE) {
+        use8x8Transform = true;
+    } else if (adaptiveTransformMode == ADAPTIVE_TRANSFORM_DISABLE) {
+        use8x8Transform = false;
+    } else {
+        // Autoselect
+        if ((profileIdc == STD_VIDEO_H264_PROFILE_IDC_INVALID) ||
+            (profileIdc >= STD_VIDEO_H264_PROFILE_IDC_HIGH)) {
+            // Unconditionally enable 8x8 transform
+            use8x8Transform = true;
+        }
+    }
+
+    // If the profileIdc hasn't been specified, force set it now.
+    if (profileIdc == STD_VIDEO_H264_PROFILE_IDC_INVALID) {
+        profileIdc = STD_VIDEO_H264_PROFILE_IDC_BASELINE;
+
+        if ((GetMaxBFrameCount() > 0) || (entropyCodingMode == ENTROPY_CODING_MODE_CABAC))
+            profileIdc = STD_VIDEO_H264_PROFILE_IDC_MAIN;
+
+        if (use8x8Transform) {
+            profileIdc = STD_VIDEO_H264_PROFILE_IDC_HIGH;
+        }
+
+        if (((tuningMode == VK_VIDEO_ENCODE_TUNING_MODE_LOSSLESS_KHR) &&
+             (rateControlMode == VK_VIDEO_ENCODE_RATE_CONTROL_MODE_DISABLED_BIT_KHR)) ||
+            (input.chromaSubsampling == VK_VIDEO_CHROMA_SUBSAMPLING_444_BIT_KHR)) {
+            profileIdc = STD_VIDEO_H264_PROFILE_IDC_HIGH_444_PREDICTIVE;
+        }
+    }
+
     uint32_t levelBitRate = ((rateControlMode != VK_VIDEO_ENCODE_RATE_CONTROL_MODE_DISABLED_BIT_KHR) && hrdBitrate == 0)
                                 ? averageBitrate // constrained by avg bitrate
                                 : hrdBitrate;  // constrained by max bitrate
@@ -472,6 +471,11 @@ int8_t EncoderConfigH264::InitDpbCount()
 
     // find lowest possible level
     levelIdc = DetermineLevel(dpbCount, levelBitRate, vbvBufferSize, frameRate);
+}
+
+int8_t EncoderConfigH264::InitDpbCount()
+{
+    dpbCount = 0; // TODO: What is the need for this?
 
     uint8_t levelDpbSize = (uint8_t)(((1024 * levelLimits[levelIdc].maxDPB)) /
                             ((pic_width_in_mbs * pic_height_in_map_units) * 384));

vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfigH264.h
@@ -187,6 +187,8 @@ struct EncoderConfigH264 : public EncoderConfig {
         return VK_ERROR_INVALID_VIDEO_STD_PARAMETERS_KHR;
     }
 
+    void InitProfileLevel();
+
     virtual VkResult InitDeviceCapabilities(const VulkanDeviceContext* vkDevCtx) override;
 
     virtual uint32_t GetDefaultVideoProfileIdc() override { return STD_VIDEO_H264_PROFILE_IDC_HIGH; };

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoderH264.cpp
@@ -45,6 +45,9 @@ VkResult VkVideoEncoderH264::InitEncoderCodec(VkSharedBaseObj<EncoderConfig>& en
         return VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR;
     }
 
+    // Initialize the codec profile and level before other encoder configuration
+    m_encoderConfig->InitProfileLevel();
+
     VkResult result = InitEncoder(encoderConfig);
     if (result != VK_SUCCESS) {
         fprintf(stderr, "\nERROR: InitEncoder() failed with ret(%d)\n", result);

commit fe1155949e5505e108ab2780eec13f799423f4bd

EncoderConfigH264: fix InitProfileLevel to depend only on user config
Commit 7734964d130c3ec03a6b1daa71f7a7f0e729922b, which introduced the
InitProfileLevel method, also introduced a bug in that code where a call
to the GOP structure's GetConsecutiveBFrameCount() method was replaced
by the GetMaxBFrameCount() method. This is problematic because
GetMaxBFrameCount() depends on device capabilities which have not been
queried yet. This commit restores the call to
GetConsecutiveBFrameCount() in the profile determination code.

This commit also removes a check on the rate control mode when lossless
encoding is requested, as the rate control mode may not have been
specified by the user (and it will be determined later when querying the
quality level properties). This change works as lossless encoding
inherently requires the HIGH_444_PREDICTIVE profile regardless of rate
control settings.

Together, these changes make it safe to call InitProfileLevel() early in
the overall initialization sequence, before any capabilities or
recommended settings have been queried.

Signed-off-by: Srinath Kumarapuram <skr@nvidia.com>

vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfigH264.cpp
@@ -464,15 +464,16 @@ void EncoderConfigH264::InitProfileLevel()
     if (profileIdc == STD_VIDEO_H264_PROFILE_IDC_INVALID) {
         profileIdc = STD_VIDEO_H264_PROFILE_IDC_BASELINE;
 
-        if ((GetMaxBFrameCount() > 0) || (entropyCodingMode == ENTROPY_CODING_MODE_CABAC))
+        // Upgrade to MAIN profile if using B-frames or CABAC entropy coding
+        if ((gopStructure.GetConsecutiveBFrameCount() > 0) || (entropyCodingMode == ENTROPY_CODING_MODE_CABAC))
             profileIdc = STD_VIDEO_H264_PROFILE_IDC_MAIN;
 
         if (use8x8Transform) {
             profileIdc = STD_VIDEO_H264_PROFILE_IDC_HIGH;
         }
 
-        if (((tuningMode == VK_VIDEO_ENCODE_TUNING_MODE_LOSSLESS_KHR) &&
-             (rateControlMode == VK_VIDEO_ENCODE_RATE_CONTROL_MODE_DISABLED_BIT_KHR)) ||
+        // Upgrade to HIGH_444_PREDICTIVE for lossless encoding or 4:4:4 chroma
+        if ((tuningMode == VK_VIDEO_ENCODE_TUNING_MODE_LOSSLESS_KHR) ||
             (input.chromaSubsampling == VK_VIDEO_CHROMA_SUBSAMPLING_444_BIT_KHR)) {
             profileIdc = STD_VIDEO_H264_PROFILE_IDC_HIGH_444_PREDICTIVE;
         }